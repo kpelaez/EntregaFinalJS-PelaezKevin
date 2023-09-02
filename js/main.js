b6c6c0fc60fa54f8fe949f129fb835a371b3d2d3
kpelaez <55926499+kpelaez@users.noreply.github.com>
2023-09-01 21:04:45 -0300

    Cambios realizados

--- js/main.js
+++ js/main.js
@@ -71,30 +71,12 @@ function mostrarPresupuestoFinal (total){
 
     }   
 
+    let contenedor_presupuesto = document.createElement("div");
     let stringTotal = `El total del presupuesto es: $${total}`;
 
-    let contenedor_presupuesto = document.createElement("div");
     contenedor_presupuesto.id = "presupuesto_carrito";
 
-    //Se toma el valor del dolar de la api del bna. 
-    const url = 'https://api.estadisticasbcra.com/usd_of_minorista';
-    const headers = new Headers({'Authorization': 'BEARER eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjUxMzA0MTgsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJwZWxhZXprZXZpbkBob3RtYWlsLmNvbSJ9.20czYdE6WVZBPThWPR3wVVCPxzU0cfY2blEfjEZGPzpqTxneLdNqmZK4HoIZhayrWmnqKQ-dZhhtYSZvBKn9NQ'});
-
-    fetch(url, {
-        method:'GET',
-        headers: headers})
-    .then(response =>{
-        if(!response.ok){
-            throw console.log("ERROR");
-        }
-        return response.json()
-    })
-    .then(data=>{
-        console.log(data);
-    })
-
-    //Agregar valor en dolares
-    let importeDolar = total/cotizacion;
+    
     //Estructura del div final
     contenedor_presupuesto.innerHTML = `<h3> ${inicioPresu}</h3>
                                         <p>${cuerpo}</p>
